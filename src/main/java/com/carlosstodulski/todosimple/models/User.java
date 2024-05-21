81ea90736f9883d2f72da14bd4291bcbed5f6b66
Carlos Stodulski <carmassera@hotmail.com>
2024-05-21 14:34:59 -0300

    Criação do User Controller

--- src/main/java/com/carlosstodulski/todosimple/models/User.java
+++ src/main/java/com/carlosstodulski/todosimple/models/User.java
@@ -15,6 +15,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -82,7 +83,8 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
+ 
+    @JsonIgnore
     public List<Task> getTasks() {
         return this.tasks;
     }
